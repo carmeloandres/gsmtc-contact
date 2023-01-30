bd11702ca5dc5a63e0ab00144802bce07816feb8
carmelo <carmeload@gmail.com>
2023-01-30 19:09:24 +0100

    first commit

---
+++ gsmtc-contact.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Gsmtc Contact
+ * Description:       Plugin for the use of static contact forms.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       gsmtc-contact
+ *
+ * @package           gsmtc-contact
+ */
+if ( ! defined( 'ABSPATH' ) ) {die;} ; // to prevent direct access
+
+if ( ! defined('PLUGIN_DIR')) define ('PLUGIN_DIR',plugin_dir_path(__FILE__));
+
